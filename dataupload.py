ca131cffcb2e0fa5e252746494cf5cd4a6ab176a
Alice Barbe <alice_barbe@hotmail.com>
2022-02-27 04:50:27 -0500

    Create dataupload.py

---
+++ dataupload.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 27 04:43:35 2022
+
+@author: Alice
+"""
+
+import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("../firebase_credentials.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+collection_name = "emotions"
+
+def upload_emotion(emotion):
+    doc_title = str(int(time.time()))
+    data = {"timestamp": doc_title,
+            "emotion": emotion}
+    db.collection(collection_name).document(doc_title).set(data)
